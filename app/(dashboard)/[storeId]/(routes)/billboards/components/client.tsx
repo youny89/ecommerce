e7c1006a47641b870325fe6541b03f68135db9c5
youny89 <youny@kakao.com>
2023-10-05 18:30:38 +0900

    Added api list & finished billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
 import { BillboardColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
+import ApiList from "@/components/ui/api-list"
 
 interface BillboardClientProps {
     data: BillboardColumn[]
@@ -28,6 +29,10 @@ export const BillboardClient:React.FC<BillboardClientProps> = ({ data }) => {
             <Separator/>
 
             <DataTable columns={columns} data={data} searchKey="label"/>
+
+            <Separator />
+            <Heading title="API" description="빌보드 API"/>
+            <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
